c2bef6ecac50c9a4b466ef3d9c1bca11e2d0757e
David Gerard <dgredsox@gmail.com>
2023-10-11 16:11:28 -0400

    add parseFrontendQuery outline

--- searchengine/src/main/java/com/formflow/searchengine/SearchEngine.java
+++ searchengine/src/main/java/com/formflow/searchengine/SearchEngine.java
@@ -15,7 +15,19 @@ public class SearchEngine {
    * @return JSON object containing all rows of information from the file metadata
    *         database that matches the selections from the input query
    */
-  public String getFileMetadata(String query) {
+  public String getFileMetadata(String frontendQuery) {
+    String sqlQuery = parseFrontendQuery(frontendQuery);
+    // Next, fetch from database and return the JSON
     return "Fetching File Metadata!";
+  }
+
+  /*
+   * Parses a frontend query that was embedded within the GET request link
+   * @param frontendQuery A query that was embedded in the original link
+   *                      ex. type=PDF&project_name=NAME ...
+   * @return String of MySQL to query the database based on the frontend query
+   */
+  private String parseFrontendQuery(String frontendQuery) {
+    return null;
   }
 }
