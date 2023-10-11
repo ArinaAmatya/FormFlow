27a9f8050e486376f2eabf65d6d6e8a8d9c1a681
David Gerard <dgredsox@gmail.com>
2023-10-11 13:37:29 -0400

    add comments and develop POST mapping for application state

--- searchengine/src/main/java/com/formflow/searchengine/Config.java
+++ searchengine/src/main/java/com/formflow/searchengine/Config.java
@@ -3,13 +3,16 @@ package com.formflow.searchengine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/* Defines the Beans for this server application to enforce max one instance of each class */
 @Configuration
 public class Config {
+  /* SearchEngine Bean */
   @Bean
   public SearchEngine searchEngine() {
     return new SearchEngine();
   }
 
+  /* SupabaseWrapper Bean */
   @Bean
   public SupabaseWrapper supabaseWrapper() {
     return new SupabaseWrapper();
--- searchengine/src/main/java/com/formflow/searchengine/FrontendController.java
+++ searchengine/src/main/java/com/formflow/searchengine/FrontendController.java
@@ -1,31 +1,45 @@
 package com.formflow.searchengine;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.MergedAnnotations.Search;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/* Controller to expose the REST API endpoints for the server */
 @RestController
 public class FrontendController {
 
   @Autowired
   public SearchEngine searchEngine;
 
-  @GetMapping("/getFileMetadata")
-  public String getFileMetadata() {
-    return searchEngine.getFileMetadata("TEST");
+  /* 
+   * Fetch file metadata rows based on a search query originating from the frontend
+   * @param query The query originating from the frontend web application
+   * */
+  @GetMapping("/getFileMetadata?{query}")
+  public String getFileMetadata(@PathVariable("query") String query) {
+    return searchEngine.getFileMetadata(query);
   }
 
+  /* 
+   * Fetch file object based on a search query originating from the frontend
+   * @param filePath The path of the object in the file store database
+   * */
   @GetMapping("/getFileObject/{filePath}")
   public String getFileObject(@PathVariable("filePath") String filePath) {
     return "Fetching File Object...";
   }
 
-  @PostMapping("/postApplicationMetadata")
-  public String postFile(@PathVariable("filePath") String filePath) {
-    return "Fetching File Object...";
+  /* 
+   * Received information on the current state of frontend web application
+   * @param state The updated state of the web application
+   * */
+  @PostMapping(path="/postApplicationState",
+    consumes = {"application/json"})
+  public String postApplicationState(@RequestBody String state) {
+    return "Posting application state data...";
   }
 
 }
