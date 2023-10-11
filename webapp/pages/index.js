745d9f710ea02b1b0f31f1f79145d5b04531e550
bholtmann <bholtmann@umass.edu>
2023-10-10 21:26:43 -0400

    Migrated to Next.js

---
+++ webapp/pages/index.js
@@ -0,0 +1,12 @@
+import Searchbar from '../components/Searchbar';
+import '../styles/search.module.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function SearchPage() {
+  return <Searchbar/>;
+}
+
+export default SearchPage;
---
+++ webapp/pages/search.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Searchbar from '../components/Searchbar';
+import '../styles/search.module.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+function SearchPage() {
+  return <Searchbar/>;
+}
+
+export default SearchPage;
