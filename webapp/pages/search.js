337e8ba16a2b1bf833986c2305004e998bff6d96
Annanta Bud <120224186+annantab@users.noreply.github.com>
2023-10-23 16:26:54 -0400

    added navbar to page

--- webapp/pages/search.js
+++ webapp/pages/search.js
@@ -1,13 +1,13 @@
-import Searchbar from '../components/Searchbar';
 import '../styles/search.module.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import NavBar from '../components/NavBar';
 
 
 function SearchPage() {
-  return <Searchbar/>;
+  return <NavBar/>;
 }
 
 export default SearchPage;
