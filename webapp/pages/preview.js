bfb846e398dd9c4619392b7771589d122733cfab
BHoltmann <brendan.holtmann@gmail.com>
2023-10-23 17:21:40 -0400

    Added FIlePreviewsPage

--- webapp/pages/preview.js
+++ webapp/pages/preview.js
@@ -3,7 +3,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import FilePreview from '../components/FilePreview';
 
 export default function FilePreviewsPage() {
-  return <NavBar/>;
+  return <FilePreview/>;
 }
