36a6612d7e3c6544e16fe2ea86bb9186d3018263
bholtmann <bholtmann@umass.edu>
2023-10-24 00:12:41 -0400

    Converted most css to tailwind

--- webapp/pages/login.js
+++ webapp/pages/login.js
@@ -1,9 +1,3 @@
-import '../styles/login.module.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 import Container from '@mui/material/Container';
 import logo from '../public/icons/logo512.png';
 import Image from 'next/image';
@@ -13,8 +7,6 @@ import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
 import { useRouter } from 'next/router'
-
-import styles from '../styles/login.module.css';
 
 export default function LoginPage() {
   const router = useRouter();
@@ -27,45 +19,47 @@ export default function LoginPage() {
   
   return (
     <>
-      <div className={styles["logo-container"]}>
-      <Image className={styles["logo-img"]}
+      <div className="text-center">
+      <Image className="ml-auto mr-auto"
         src={logo}
         width={300}
         height={300}
         alt="Formflow Logo"
       />
       </div>
-      <div className={styles["login-container"]}>
-        <div className={styles["login-prompt-text-container"]}>
-          <p className={styles["login-prompt-text"]}>Login</p>
+      <div className="w-screen flex justify-center">
+        <div className="h-[400px] w-[500px] text-center bg-theme-contrast-blue-light border-[#555555] border-2 rounded-[40px] shadow-[#555555] shadow-md">
+          <div>
+            <p className="text-5xl mt-[30px]">Login</p>
+          </div>
+          <br/>
+          <TextField className="m-[3px] w-[400px]"
+            id="outlined-basic"
+            label="User ID"
+            variant="outlined"
+          />
+          <br/>
+          <TextField className="m-[3px] w-[400px]"
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+          />
+          <br/>
+          <FormControlLabel
+            control={
+              <Checkbox />
+            }
+            label=" Stay logged in" />
+          <br/>
+          <Button className="mt-[20px] h-[40px] w-[300px] bg-theme-logo-blue"
+            variant="contained"
+            onClick={tryLogin}
+          > Login </Button>
+          <br/>
+          <Link className="inline-block pt-[15px]"
+            href="#"
+          > forgot your password? </Link>
         </div>
-        <br/>
-        <TextField className={styles["user-id-input"]}
-          id="outlined-basic"
-          label="User ID"
-          variant="outlined"
-        />
-        <br/>
-        <TextField className={styles["password-input"]}
-          id="outlined-basic"
-          label="Password"
-          variant="outlined"
-        />
-        <br/>
-        <FormControlLabel className={styles["stay-logged-in-label"]}
-          control={
-            <Checkbox className={styles["stay-logged-in-checkbox"]} />
-          }
-          label=" Stay logged in" />
-        <br/>
-        <Button className={styles["login"]}
-          variant="contained"
-          onClick={tryLogin}
-        > Login </Button>
-        <br/>
-        <Link className={styles["forgot-password"]}
-          href="#"
-        > forgot your password? </Link>
       </div>
     </>
   );
--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-logo-blue': '#0084c6',
+        'theme-bg-blue': '#e4eaf1',
+        'theme-contrast-blue-dark': '#b7cbe2',
+        'theme-contrast-blue-light': '#cad8e7',
+        'theme-grey': '#dddddd'
+      }
+    },
   },
   plugins: [],
 }
