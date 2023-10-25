bd67a22ee973700825d87ba6dc1a57e74912afe9
Ria Sarkar <jitasark@gmail.com>
2023-10-25 13:33:23 -0400

    "added advanced search to components"

---
+++ webapp/components/AdvancedSearch.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function FormPropsTextFields() {
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          id="filled-search"
+          label="Project Name/ID"
+          type="search"
+          variant="filled"
+        />
+        <TextField
+          id="filled-search"
+          label="Proposal Name/ID"
+          type="search"
+          variant="filled"
+        />
+         <TextField
+          id="filled-search"
+          label="File Name"
+          type="search"
+          variant="filled"
+        />
+         <TextField
+          id="filled-search"
+          label="Auction Type"
+          type="search"
+          variant="filled"
+        />
+         <TextField
+          id="filled-search"
+          label="File Type"
+          type="search"
+          variant="filled"
+        />
+        <TextField
+          id="filled-search"
+          label="File Size"
+          type="search"
+          variant="filled"
+        />
+      </div>
+    </Box>
+  );
+}
