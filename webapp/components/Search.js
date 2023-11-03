5e5643c0be3669a5b4291e4da264a1e2c392b7c8
BHoltmann <brendan.holtmann@gmail.com>
2023-11-03 14:25:53 -0400

    Added filter buttons

--- webapp/components/Search.js
+++ webapp/components/Search.js
@@ -192,7 +192,11 @@ export default function Search() {
           </IconButton>
         </DrawerHeader>
         <Divider />
-        <Accordion disableGutters elevation={0} className="bg-theme-grey-light">
+        <Accordion className="bg-theme-grey-light"
+            disableGutters
+            elevation={0}
+            defaultExpanded
+        >
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1a-content"
@@ -399,6 +403,18 @@ export default function Search() {
                 </svg>
               </Button>
             </div>
+            <Button className="h-[56px] w-[316px] mt-[13px] ml-[13px] shadow shadow-theme-logo-blue rounded-xl bg-theme-contrast-blue-light text-xl font-extrabold hover:bg-[#afc3da] hover:border-none"
+                onClick={search}
+            >
+                <svg className="h-[32px] w-[32px] fill-theme-logo-blue mr-[20px]"
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 512 512"
+                >
+                    <path d="M3.9 54.9C10.5 40.9 24.5 32 40 32H472c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9V448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6V320.9L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z"/>
+                </svg>
+                Filter & Search
+            </Button>
           </AccordionDetails>
         </Accordion>
         <Divider />
